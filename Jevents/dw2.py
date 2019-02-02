fbc8f8de9cfa5d4b91879231b5d14bb4a1df3269
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-02-02 23:57:26 +0800

    Changes: 1.	Fully finnished building the Callback Class that acts as a wrapper on top 	of the list that contains all the callbacks. Fixed some issues with the 	class and made it into a iterable itself.
    2.	Updated the test script of above module to reflect all the new changes
    	including the use of magic/dunder methods for the callback lists.

--- Jevents/dw2.py
+++ Jevents/dw2.py
@@ -8,8 +8,9 @@ class Watch:
     def __init__(self, data):
         """ Pass constructor the data item to be watched """
         self.__data = data
-        self.on_set = callbacks()
-        self.on_change = callbacks()
+        # Create 2 Callback objects to store all the callback functions
+        self.on_set = Callback()
+        self.on_change = Callback()
 
     # Method to set a value to the data variable watched by this class
     def set(self, data):
@@ -17,10 +18,10 @@ class Watch:
         if data != self.__data:  # Check for data equality, so will not work for object references
             self.__data = data
             # Call all the __on_change_cbs callback functions
-            self.__event(self.on_change.get)
+            self.__event(self.on_change)
 
         # Regardless of data, call all the __on_set_cbs callback functions when set method called.
-        self.__event(self.on_set.get)
+        self.__event(self.on_set)
         # Return self reference to allow method call chainings.
         return self
 
@@ -29,54 +30,69 @@ class Watch:
     def value(self):
         return self.__data
 
-    # Method to append a new callback function to be ran when the set method is called
-    # def on_set(self, cb):
-    #     # self.__on_set_cbs.append(cb)
-    #     # Return self reference to allow method call chainings.
-    #     return self
-
-    # Method to append a new callback function to be ran when the watched data is changed
-    # def on_change(self, cb):
-    #     self.__on_change_cbs.append(cb)
-    #     # Return self reference to allow method call chainings.
-    #     return self
-
-    # def removeListener(self, cb=None):
-    #     # To implement the second part where only the specified callback function is removed.
-    #     # self.__cbs.clear()  # Not sure if this method works, needs to be tested
-    #     # Return self reference to allow method call chainings.
-    #     return self
-
     # "Hidden" method that is called when the data is changed, to run all the given callbacks in seperate threads
     def __event(self, callbacks):
         # Loop through and run all the callbacks as seperate threads
         for cb in callbacks:
             Thread(target=cb, daemon=True, args=(self.__data,)).start()
 
-    """ Allow user to do w(5) to pass set method the value 5, where w = Watch(1) """
+    # Method to return the string representation when str(obj) called
+    def __str__(self):
+        # 2 different ways to get the string representation.
+        # Method 1 better for objects for their string representation
+        return str(self.__data)
+        # Method 2 better for primitive data types
+        # return '{}'.format(self.__data)
+
+    # Allow user to call w to get data instead of w.value, where w = Watch(1)
+    __repr__ = value
+    # Allow user to do w(5) to pass set method the value 5, where w = Watch(1)
     __call__ = set
 
 
-class callbacks:
-    def __init__(self):
-        # __on_set_cbs callback functions run the moment set method is used to set a value to the variable.
-        self.__cbs = []
+class Callback:
+    """
+        For the 'append' and 'remove' methods
+        Return self is NEEDED to allow user to call "data.on_set_callbacks += new_cb"
+        This is because if nothing is returned, the on_set_callbacks property of the data object
+        will be overwritten to point to new_cb instead of the callbacks with new_cb inside.
+    """
+    def __init__(self, cbs=None):
+        """ Able to accept pre-constructed lists of callback functions """
+        if cbs == None:
+            # Create an empty list for this Callback object if none given
+            self.__cbs = []
+        else:
+            self.__cbs = cbs
 
+    # Method to append data/callback-functions into the list
     def append(self, cb):
         self.__cbs.append(cb)
+        # Return self reference to allow method call chainings.
         return self
 
+    # Method to remove data/callback-functions from the list
     def remove(self, cb):
-        pass
+        # Return self reference to allow method call chainings.
+        return self
+
+    def __len__(self):
+        return len(self.__cbs)
 
     @property
     def get(self):
         return self.__cbs
 
-    # __add__ = append
-    # __sub__ = remove
+    # Allow user to use these dunder methods to do +, -, +=, -= operations on this class of objects
+    __add__ = __iadd__ = append
+    __sub__ = __isub__ = remove
 
-    __iadd__ = append
-    __isub__ = remove
-
+    # Shorthand to allow user to set value by calling callback(value)
     __call__ = append
+    # Allow user to get the list when called directly, by using the get method
+    __repr__ = get
+    
+    # Shorthand to allow user to get the iterable by calling Callback_list instead of Callback_list.value
+    def __iter__(self):
+        # By using the iterable form from the list itself, __next__ does not need to be implemented
+        return iter(self.__cbs)
--- Jevents/dw2_test.py
+++ Jevents/dw2_test.py
@@ -8,10 +8,12 @@ import threading
 if __name__ == "__main__":
     # Create a new data variable and store in the watchData object
     sensorData = Watch(12)
-    # The data stored in the object can only be accessed via the value property
+    # The data stored in the object can be accessed via the value property
     print(sensorData.value)
+    # Alternatively it can also be accessed by calling itself with the help of the __repr__
+    print(sensorData)
 
-    # Below are 3 different callbacks that should run when the data changes
+    # Below are 4 different callbacks that should run when the data changes
     def hi(data):
         sleep(2.5)
         print('hello world ', data)
@@ -24,34 +26,39 @@ if __name__ == "__main__":
     # The input arguemnts is a tuple, so below is another way of accessing the data.
     def on_change_cb(*data):
         print('The value has been changed ', data[0])
+    
+    def on_change_cb2(data):
+        sleep(4)
+        print('On change Callback function 2 ', data)
 
-    # Add the callbacks to the object
+    # Add the callbacks to the object using the on_set method
     sensorData.on_set(hi)
-    sensorData.on_set(chicken)
-    # sensorData.on_change(on_change_cb)
-    # Alternative way using magic methods to set/append an on_change callback function
+    # Alternative way using __add__ magic method to set/append an on_set callback function
+    sensorData.on_set + chicken
+    # Alternative way using __iadd__ magic method to set/append an on_change callback function
     sensorData.on_change += on_change_cb
+    sensorData.on_change += on_change_cb2
 
     # Add a time delay to simulate real life operations
     sleep(1.4)
     # Update the data in the object, this will cause all the callbacks to be called.
     sensorData(1)
     # Print out the updated value stored in the object.
-    print(sensorData.value)
+    print(sensorData)
 
     # Add a time delay to simulate real life operations
     sleep(1.4)
     # Below shows the alternative way to call the set method instead of the __call__ shorthand
     sensorData.set(2)
     # Print out the updated value stored in the object.
-    print(sensorData.value)
+    print(sensorData)
 
     # Add a time delay to simulate real life operations
     sleep(1.4)
     # Update the data in the object, this will cause all the callbacks to be called.
     sensorData(2)
     # Print out the updated value stored in the object.
-    print(sensorData.value)
+    print(sensorData)
 
     # Call the wait function to stop main thread from ending before the daemonic threads finnish
     wait_for_daemons()
